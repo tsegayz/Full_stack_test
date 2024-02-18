2251f19bb8c80f87e568ca400c994103ca988784
Tsega Yaekob <tsegayaekob7@gmail.com>
2024-02-18 16:26:49 +0300

    chat ui

--- sendbird/lib/main.dart
+++ sendbird/lib/main.dart
@@ -1,210 +1,148 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ChatDetail(););
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ChatDetail(),
+  ));
 }
 
 class ChatDetail extends StatelessWidget {
-  final String name;
-  final String chatId;
-  final messageCtrl = TextEditingController();
-  ChatDetail(this.name, this.chatId);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 239, 239, 239),
-      appBar: AppBar(
-        toolbarHeight: 70,
-        title: Text(
-          name,
-          style: TextStyle(color: Colors.black, fontSize: 20),
-        ),
-        backgroundColor: Colors.grey[200],
-        elevation: 2.0,
-      ),
-      body: Column(children: [
-        Expanded(
-          child: BlocBuilder<AuthBloc, AuthState>(
-            builder: (_, authState) {
-              if (!(authState is LoginSuccessful)) {
-                return Text("You are not Logged in!");
-              }
-              return BlocConsumer<MessageBloc, MessageState>(
-                  listener: (_, state) {
-                if (state is MessageSentSuccessful) {
-                  final messageBloc = BlocProvider.of<MessageBloc>(context);
-                  messageBloc.add(LoadMessages(this.chatId));
-                }
-              }, builder: (context, state) {
-                if (state is MessageLoading) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                if (state is SendingMessage) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                if (state is MessagesLoadingFailure) {
-                  return Center(
-                    child: Text("Couldn't load messages"),
-                  );
-                }
-                if (state is MessageSendingFailure) {
-                  return Center(
-                    child: Text("Couldn't Send message."),
-                  );
-                }
-                if (state is MessagesLoadSuccess) {
-                  return state.messages.isNotEmpty
-                      ? ListView.builder(
-                          reverse: true,
-                          itemCount: state.messages.length,
-                          itemBuilder: (_, index) => Align(
-                              alignment: authState.userId.id ==
-                                      state.messages[index].owner
-                                  ? Alignment.centerRight
-                                  : Alignment.centerLeft,
-                              child: Container(
-                                  margin: EdgeInsets.all(10),
-                                  color: authState.userId.id ==
-                                          state.messages[index].owner
-                                      ? Colors.indigoAccent
-                                      : Colors.black12,
-                                  // decoration: BoxDecoration(
-                                  //   shape: BoxShape.rectangle,
-                                  //   borderRadius: BorderRadius.circular(50),),
-                                  // decoration: BoxDecoration( borderRadius: BorderRadius.circular(50)),
-                                  // decoration: BoxDecoration(borderRadius: BorderRadiusGeometry()),
-                                  // color: Colors.black12/,
-                                  padding: EdgeInsets.all(20),
-                                  child: Text(state.messages[index].text))))
-                      : Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Center(
-                              child: Container(
-                                child: CircleAvatar(
-                                  backgroundColor: Colors.indigoAccent,
-                                  radius: 50,
-                                  child: Icon(
-                                    Icons.sentiment_very_dissatisfied,
-                                    size: 100,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                height: 200,
-                                width: 200,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(100),
-                                  shape: BoxShape.rectangle,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Center(
-                              child: Text(
-                                "The User doesn't exist.",
-                                style: TextStyle(
-                                  color: Colors.indigoAccent,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 23,
-                                ),
-                              ),
-                            )
-                          ],
-                        );
-                }
-                return Text("Never Come Here");
-              });
-            },
-          ),
-        ),
-        //message to be sent
-        Align(
-          alignment: Alignment.bottomLeft,
-          child: Padding(
-            padding: const EdgeInsets.all(
-              8,
-            ),
-            child: Container(
-              padding: EdgeInsets.only(left: 10),
-              height: 55,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(16)),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      controller: messageCtrl,
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 0, 0, 0), fontSize: 20),
-                      decoration: InputDecoration(
-                          hintText: "Type message.........",
-                          hintStyle:
-                              TextStyle(color: Colors.black87, fontSize: 18),
-                          border: InputBorder.none),
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 3, 3, 3),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 19, 18, 18),
+          titleSpacing: 0,
+          title: Stack(
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    padding: EdgeInsets.only(left: 20),
+                    icon: Icon(
+                      Icons.arrow_back_ios,
+                      color: Color.fromARGB(255, 88, 88, 88),
+                      size: 15,
                     ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                   ),
                   SizedBox(
-                    height: 40,
-                    child: BlocBuilder<AuthBloc, AuthState>(
-                      builder: (context, state) {
-                        if (state is! LoginSuccessful) {
-                          return Text("You havent Logged in");
-                        }
-                        return BlocBuilder<MessageBloc, MessageState>(
-                          builder: (context, messageState) {
-                            if (messageState is MessagesLoadSuccess) {
-                              return FloatingActionButton(
-                                backgroundColor: Colors.white,
-                                onPressed: () {
-                                  final messageBloc =
-                                      BlocProvider.of<MessageBloc>(context);
-                                  messageBloc.add(
-                                    CreateMessage(
-                                      this.chatId,
-                                      Message(
-                                        text: messageCtrl.text,
-                                        owner: state.userId.id!,
-                                        chat: this.chatId,
-                                        time: DateTime.now(),
-                                      ),
-                                    ),
-                                  );
-                                },
-                                child: Icon(
-                                  Icons.send,
-                                  color: Color.fromARGB(255, 31, 97, 249),
-                                  size: 20,
+                    width: 130,
+                  ),
+                  Text(
+                    'username',
+                    style: TextStyle(
+                      fontFamily: 'OrelegaOne',
+                      fontSize: 20,
+                      color: const Color.fromARGB(255, 143, 143, 143),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Color.fromARGB(255, 88, 88, 88),
+                size: 18,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            SizedBox(width: 10),
+          ],
+        ),
+        body: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.add,
+                      color: Color.fromARGB(255, 143, 143, 143),
+                      size: 25,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Color.fromARGB(255, 88, 88, 88)), // Add gray border here
+                        borderRadius: BorderRadius.circular(36),
+                      ),
+                      child: Container(
+                        height: 55,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 32, 32, 32),
+                          borderRadius: BorderRadius.circular(36),
+                        ),
+                        child: Row(
+                          children: [
+                            SizedBox(width: 20,),
+                            Expanded(
+                              child: TextField(
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  fontSize: 20,
                                 ),
-                              );
-                            }
-                            return FloatingActionButton(
-                              backgroundColor: Colors.white,
-                              onPressed: () {},
-                              child: Icon(
-                                Icons.send,
-                                color: Color.fromARGB(255, 31, 97, 249),
-                                size: 20,
+                                decoration: InputDecoration(
+                                  hintText: "Type message.........",
+                                  hintStyle: TextStyle(
+                                    color: Color.fromARGB(239, 90, 90, 90),
+                                    fontSize: 18,
+                                  ),
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.symmetric(
+                                      vertical:
+                                          12), // Adjust vertical padding here
+                                ),
                               ),
-                            );
-                          },
-                        );
-                      },
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 8.0),
+                              child: SizedBox(
+                                height: 30,
+                                width: 30,
+                                child: FloatingActionButton(
+                                  backgroundColor:
+                                      Color.fromARGB(255, 65, 64, 64),
+                                  onPressed: () {},
+                                  child: Icon(
+                                    Icons.arrow_upward,
+                                    color: Color.fromARGB(255, 38, 38, 39),
+                                    size: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
           ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
